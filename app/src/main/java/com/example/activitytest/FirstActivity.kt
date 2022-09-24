00d4e9c5715ed68ec6987c2b5e1258bf58c9318b
Zhang <2587225040@qq.com>
2022-09-24 21:02:06 +0800

    have a try

---
+++ app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -0,0 +1,69 @@
+package com.example.activitytest
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+
+@Suppress("DEPRECATION")
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.first_layout)
+        val button1:Button = findViewById(R.id.button1)
+        val baidu:Button = findViewById(R.id.baidu)
+        val button2:Button = findViewById(R.id.button2)
+        /*
+        button1.setOnClickListener {    //修改，因为需要双项匹配
+            val intent = Intent("com.example.activitytest.ACTION_START")
+            intent.addCategory("com.example.activitytest.MY_CATEGORY")
+            startActivity(intent)
+        }
+        */
+        button1.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
+        baidu.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.baidu.com")
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show()
+            R.id.login -> Toast.makeText(this, "Trying to login", Toast.LENGTH_LONG).show()
+            R.id.quit -> finish()
+        }
+        return true
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            1 -> if (resultCode == RESULT_OK) {
+                val returnedData = data?.getStringExtra("data_return")
+                Log.d("FirstActivity", "returned data is $returnedData")
+            }
+        }
+    }
+}
