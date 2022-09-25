b9d3fa1a17673eae5834bf546caed41f55c40c5f
litble <litble@stu.pku.edu.cn>
2022-09-25 17:37:20 +0800

    使用mybatis将本地数据库连接进去了，要改成远程数据库可能需要开放防火墙，之后弄

--- BackEnd/src/main/java/org/example/App.java
+++ BackEnd/src/main/java/org/example/App.java
@@ -1,16 +1,22 @@
+
 package org.example;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"org.example"})
 @RestController
+@MapperScan("org.example.dao")
 public class App {
 
     @RequestMapping("/")
     public String hello() {
+
         return "Hello World!";
     }
 
