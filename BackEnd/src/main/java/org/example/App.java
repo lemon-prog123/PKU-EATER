7fc1e07920e4b062e301c732beb268bdd145df43
FKL <litble@qq.com>
2022-09-26 22:12:52 +0800

    修改mysql-connector-Java的版本号

--- BackEnd/src/main/java/org/example/App.java
+++ BackEnd/src/main/java/org/example/App.java
@@ -1,6 +1,8 @@
 
 package org.example;
 
+import org.example.dao.UserDOMapper;
+import org.example.dataobject.UserDO;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -14,10 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @MapperScan("org.example.dao")
 public class App {
 
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String hello() {
-
-        return "Hello World!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null) {
+            return "用户不存在.";
+        }
+        else {
+            return userDO.getName();
+        }
     }
 
     public static void main(String[] args) {
