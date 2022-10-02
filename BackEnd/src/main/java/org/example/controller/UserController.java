d9e74656634fd5ed43cd25443c5949672086ca79
mjy <tenderne@stu.pku.edu.cn>
2022-10-02 19:58:45 +0800

    定义CommonReturnType,用对应的status加Object data的方式返回所有的json序列化方式的固定对象供前端解析使用，定义BusinessError统一管理错误码，定义BaseController解决未被controller层吸收的错误码， 用errCode加errMsg的方式表达所有内部异常

--- BackEnd/src/main/java/org/example/controller/UserController.java
+++ BackEnd/src/main/java/org/example/controller/UserController.java
@@ -1,32 +1,45 @@
 package org.example.controller;
 
 import org.example.controller.viewobject.UserVO;
+import org.example.error.BusinessException;
+import org.example.error.EmBusinessError;
 import org.example.response.CommonReturnType;
 import org.example.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.example.service.UserService;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpServerErrorException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 //如果不加ResponseBody的话
 //就要用RestController
 @Controller("user")
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
     @RequestMapping("/get")
     @ResponseBody
-    public CommonReturnType getUser(@RequestParam(name="id")Integer id){
+    public CommonReturnType getUser(@RequestParam(name="id")Integer id) throws BusinessException {
         //调用service服务获取对应id的用户对象并返回给前端
        UserModel userModel = userService.getUserById(id);
 
+       //若获取的对应用户信息不存在
+        if(userModel == null){
+
+            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+        }
+
        //将核心领域模型用户对象转化为可供UI使用的viewobject
        UserVO userVO =  convertFromModel(userModel);
+
 
        //返回通用对象
        return CommonReturnType.create(userVO);
@@ -40,4 +53,25 @@ public class UserController {
         BeanUtils.copyProperties(userModel, userVO);
         return userVO;
     }
+
+
+    //定义exceptionhandler解决未被controller层吸收的exception
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Object handlerException(HttpServletRequest request, Exception ex){
+        Map<String,Object> responseData = new HashMap<>();
+        if(ex instanceof BusinessException){
+            BusinessException businessException = (BusinessException)ex;
+            responseData.put("errCode",businessException.getErrCode());
+            responseData.put("errMsg",businessException.getErrMsg());
+        }else{
+            responseData.put("errCode",EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg",EmBusinessError.UNKNOWN_ERROR.getErrMsg());
+        }
+        return CommonReturnType.create(responseData,"fail");
+    }
+
+
+
 }
