b585bcda4031d923cf58d9a2bffe1c69ee05a73b
mjy <tenderne@stu.pku.edu.cn>
2022-12-02 16:21:22 +0800

    使用了凯撒密码对收到的报文进行二次加密存入数据库中

--- BackEnd/src/main/java/org/example/controller/UserController.java
+++ BackEnd/src/main/java/org/example/controller/UserController.java
@@ -21,6 +21,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
 //如果不加ResponseBody的话
 //就要用RestController
 @Controller("user")
@@ -83,6 +85,69 @@ public class UserController extends BaseController {
         return CommonReturnType.create(responseData,"fail");
     }
 
+    /***
+     * 使用凯撒加密方式加密数据
+     * @param orignal 原文
+     * @param key 密钥
+     * @return 加密后的字符
+     */
+
+    private static String encryptKaisa(String orignal, int key) {
+        //将字符串转换为数组
+        char[] chars = orignal.toCharArray();
+        StringBuffer buffer = new StringBuffer();
+        //遍历数组
+        for(char aChar : chars) {
+            //获取字符的ASCII编码
+            int asciiCode = aChar;
+            //偏移数据
+            asciiCode *= key;
+            asciiCode = asciiCode%128;
+            //将偏移后的数据转为字符
+            char result = (char)asciiCode;
+            //拼接数据
+            buffer.append(result);
+        }
+        return buffer.toString();
+    }
+
+    /**
+     * 使用凯撒加密方式解密数据
+     *
+     * @param encryptedData :密文
+     * @param key           :密钥
+     * @return : 源数据
+     */
+    private static String decryptKaiser(String encryptedData, int key) {
+        // 将字符串转为字符数组
+        char[] chars = encryptedData.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        // 遍历数组
+        for (char aChar : chars) {
+            // 获取字符的ASCII编码
+            int asciiCode = aChar;
+            // 偏移数据
+            asciiCode -= key;
+            // 将偏移后的数据转为字符
+            char result = (char) asciiCode;
+            // 拼接数据
+            sb.append(result);
+        }
+
+        return sb.toString();
+    }
+
+    /*验证操作
+    public static void main(String[] args) {
+        String str = "open fire";
+        String encode = encryptKaisa(str, 3);
+        System.out.println("加密后："+encode);
+
+        String decode = decryptKaiser(encode, 3);
+        System.out.println("解密后："+decode);
+
+    }*/
+
     /**
      * 用户注册接口
      * 接收参数统一使用字符串，接收后再进行类型转换
@@ -93,16 +158,16 @@ public class UserController extends BaseController {
      */
     //定义请求的方式（POST而不是GET），以及表单上传编码格式（application/x-www-form-urlencoded）
     @RequestMapping(value = "/register", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
-    //@RequestMapping(value = "/register")
+    //@RequestMapping(value = "/register", method = {RequestMethod.GET})    //测试用
     @ResponseBody
     public CommonReturnType register(@RequestParam(required=false, name="name")String name,
                                      @RequestParam(required=false, name="password")String password
     ) throws BusinessException{
         UserModel userModel = new UserModel();
         userModel.setName(name);
-        //密码按道理是需要加密的，目前暂不加密
-        //userModel.setEncrptPassword(MD5Encoder.encode(password.getBytes()));
-        userModel.setEncrptPassword(password);
+        //密码加密
+        userModel.setEncrptPassword(encryptKaisa(password, 13));  //返回为空的密码
+        //userModel.setEncrptPassword(password);   //明文存储密码
 
         Integer id = userService.register(userModel);
         userModel = userService.getUserById(id);
@@ -121,6 +186,7 @@ public class UserController extends BaseController {
      * @return 通用返回对象
      */
     @RequestMapping(value = "/login", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    //@RequestMapping(value = "/login", method = {RequestMethod.GET})   //测试用
     @ResponseBody
     public CommonReturnType login(@RequestParam(required=false, name="name")String name,
                                   @RequestParam(required=false, name="password")String password)
@@ -133,7 +199,8 @@ public class UserController extends BaseController {
         }
 
         // 用户登录服务，校验登录是否合法
-        UserModel userModel = userService.validateLogin(name, password);
+        UserModel userModel = userService.validateLogin(name, encryptKaisa(password, 13));    //加密后
+        //UserModel userModel = userService.validateLogin(name, password);   //未加密
 
         // 将登录凭证加入到用户登录成功的session内
         // 切换web页面的时候，可以不用重复登录
